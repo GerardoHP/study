83809edc5346d74c137bb453a0f8859228ff9462
Gerardo Hernandez <gerardo@hernandez.perales.dev>
2023-03-15 09:59:35 -0600

    refactor the maxheap

--- heaps/maxHeaps.go
+++ heaps/maxHeaps.go
@@ -3,9 +3,7 @@ package heaps
 import "fmt"
 
 // MaxHeap struct has a slice that holds the array
-type MaxHeap struct {
-	array []int
-}
+type MaxHeap []int
 
 func NewMaxHeap() *MaxHeap {
 	return &MaxHeap{}
@@ -22,27 +20,27 @@ func NewMaxHeapFromSlice(slc []int) *MaxHeap {
 
 // Insert adds on element to the heap
 func (h *MaxHeap) Insert(key int) {
-	h.array = append(h.array, key)
-	h.maxHeapifyUp(len(h.array) - 1)
+	*h = append(*h, key)
+	h.maxHeapifyUp(len(*h) - 1)
 }
 
 // Extract returns the largest key and removes it from the heap
 func (h *MaxHeap) Extract() int {
-	extracted := h.array[0]
-	l := len(h.array) - 1
+	extracted := (*h)[0]
+	l := len((*h)) - 1
 	if l == -1 {
 		fmt.Println("cannot extract because array length is 0")
 		return -1
 	}
 
-	h.array[0] = h.array[l]
-	h.array = h.array[:l]
+	(*h)[0] = (*h)[l]
+	(*h) = (*h)[:l]
 	h.maxHeapifyDown(0)
 	return extracted
 }
 
 func (h *MaxHeap) maxHeapifyUp(index int) {
-	for p := parent(index); h.array[p] < h.array[index]; {
+	for p := parent(index); (*h)[p] < (*h)[index]; {
 		h.swap(p, index)
 		index = p
 		p = parent(index)
@@ -50,20 +48,20 @@ func (h *MaxHeap) maxHeapifyUp(index int) {
 }
 
 func (h *MaxHeap) maxHeapifyDown(index int) {
-	lastIndex := len(h.array) - 1
+	lastIndex := len((*h)) - 1
 	l, r := left(index), right(index)
 	childToCompare := 0
 	for l <= lastIndex {
 		switch {
 		case l == lastIndex:
 			childToCompare = l
-		case h.array[l] > h.array[r]:
+		case (*h)[l] > (*h)[r]:
 			childToCompare = l
 		default:
 			childToCompare = r
 		}
 
-		if h.array[index] < h.array[childToCompare] {
+		if (*h)[index] < (*h)[childToCompare] {
 			h.swap(index, childToCompare)
 			index = childToCompare
 			l, r = left(index), right(index)
@@ -74,7 +72,7 @@ func (h *MaxHeap) maxHeapifyDown(index int) {
 }
 
 func (h *MaxHeap) swap(i1, i2 int) {
-	h.array[i1], h.array[i2] = h.array[i2], h.array[i1]
+	(*h)[i1], (*h)[i2] = (*h)[i2], (*h)[i1]
 }
 
 func parent(i int) int {
